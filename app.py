0792a585c7f22a9df6a86d5225123097b2cac3ec
NoahCardoza <noahcardoza@gmail.com>
2020-10-06 14:34:32 -0700

    created hello world template

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html', client_ip=request.remote_addr)
+
 
 if __name__ == '__main__':
     app.run()
