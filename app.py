d5f261b1e1bf620ff6c8188481fa35bdaed86662
NoahCardoza <noahcardoza@gmail.com>
2020-10-06 17:55:28 -0700

    add logo and serve assets dir

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
+
+
+@app.route('/assets/<path:path>')
+def send_js(path):
+    return send_from_directory('assets', path)
 
 
 @app.route('/')
