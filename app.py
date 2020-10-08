ca3ea46980092810ca703602367b6d1019cd0fb8
Noah Cardoza <noahcardoza@gmail.com>
2020-10-08 10:56:23 -0700

    Simplified upload_file (#2)
    * complete upload function
    
    * ignore uploads folder
    
    * set button type to submit
    
    * explicitly name file input
    
    * make upload dir relative to project
    
    * access newly named file input
    
    * lint
    
    * removed virtually unused UPLOAD_FOLDER in favor of `target`
    
    * cleaned up client_ip from my hello world tests
    
    * simplified upload_file
    
    * only accept post requests
    * moved UPLOAD_DIR checking to `app_setup`
    
    * fixed typo
    
    Co-authored-by: Calvin Nguyen <calvin.nvqc@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template, request, send_from_directory
+import os
+from flask import Flask, render_template, request, send_from_directory, redirect
 from flask_bootstrap import Bootstrap
+
+from werkzeug.utils import secure_filename
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+UPLOAD_FORM_IMAGE_PARAM = 'image'
+UPLOAD_DIR = os.path.join(APP_ROOT, 'uploads')
+
 
 app = Flask(__name__)
 Bootstrap(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/assets/<path:path>')
@@ -12,8 +26,42 @@ def send_js(path):
 
 @app.route('/')
 def hello():
-    return render_template('index.html', client_ip=request.remote_addr)
+    return render_template('index.html')
+
+
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    if UPLOAD_FORM_IMAGE_PARAM not in request.files:
+        # flash('No file part')
+        return redirect(request.url)
+
+    file = request.files[UPLOAD_FORM_IMAGE_PARAM]
+
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        # flash('No selected file')
+        return redirect(request.url)
+
+    if not file or not allowed_file(file.filename):
+        # flash('No selected file')
+        return redirect(request.url)
+
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(UPLOAD_DIR, filename))
+
+    # TODO: Need to integrate with @Yida's BentoML Services
+
+    return render_template('result.html')
+
+
+def app_setup():
+    # If folder does not exist, mkdir the folder
+
+    if not os.path.isdir(UPLOAD_DIR):
+        os.mkdir(UPLOAD_DIR)
 
 
 if __name__ == '__main__':
+    app_setup()
     app.run()
