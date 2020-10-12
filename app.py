0befd3233c837221fc1cb8d647ac83d96715937b
Calvin Nguyen <calvin.nvqc@gmail.com>
2020-10-11 20:50:57 -0700

    Integrate BentoML with Flask + Jinja (#8)
    * integrating bentoml
    
    * add flask-caching to deps
    
    * updated results template
    
    * add processing template
    
    * ml save image with input file name
    
    * ignore segmentData folder
    
    * call bentoml via nonblocking Popen and poll for process finished
    
    * remove comments
    
    * redirect bentoml stdout to DEVNULL
    
    * removed print statements for testing
    
    Co-authored-by: NoahCardoza <noahcardoza@gmail.com>

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 import os
-from flask import Flask, render_template, request, send_from_directory, redirect
+from flask import Flask, render_template, request, send_from_directory, redirect, jsonify
 from flask_bootstrap import Bootstrap
-
+from flask_caching import Cache
 from werkzeug.utils import secure_filename
+import requests
+from subprocess import Popen, DEVNULL
+from uuid import uuid4
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 UPLOAD_FORM_IMAGE_PARAM = 'image'
-UPLOAD_DIR = os.path.join(APP_ROOT, 'uploads')
+UPLOAD_DIR = 'assets/uploads'
+WEB_REL_UPLOAD_DIR = '/' + UPLOAD_DIR
+ABS_UPLOAD_DIR = os.path.join(APP_ROOT, UPLOAD_DIR)
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "simple",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
+tasks = {}
 
 app = Flask(__name__)
+app.config.from_mapping(config)
+
+# Configure Bootstrap with Python
 Bootstrap(app)
+
+# clear Cache whenever re-run the server
+cache = Cache(app)
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/assets/<path:path>')
@@ -29,38 +47,70 @@ def hello():
 
 
 @app.route('/upload-file', methods=['POST'])
+# @cache.cached(timeout=300)  # cache this image result for 5 minutes
 def upload_file():
     if UPLOAD_FORM_IMAGE_PARAM not in request.files:
-        # flash('No file part')
         return redirect(request.url)
 
     file = request.files[UPLOAD_FORM_IMAGE_PARAM]
 
-    # if user does not select file, browser also
-    # submit an empty part without filename
+    # if user does not select file, browser also submit an empty part without filename
     if file.filename == '':
-        # flash('No selected file')
         return redirect(request.url)
 
     if not file or not allowed_file(file.filename):
-        # flash('No selected file')
         return redirect(request.url)
 
     filename = secure_filename(file.filename)
-    file.save(os.path.join(UPLOAD_DIR, filename))
+    absolute_filepath = os.path.join(ABS_UPLOAD_DIR, filename)
+    file.save(absolute_filepath)
 
-    # TODO: Need to integrate with @Yida's BentoML Services
+    # TODO: Need to integrate with @Yida's BentoML Services. Call BentoML API
 
-    return render_template('result.html')
+    pid = str(uuid4())
+
+    process = Popen(['bentoml', 'run', 'PytorchImageSegment:latest',
+                     'predict',  f'--input-file={absolute_filepath}'], cwd=os.path.join(APP_ROOT, 'img_process_backend'), stdout=DEVNULL)
+
+    tasks[pid] = {
+        'filename': filename,
+        'process': process
+    }
+
+    return redirect(f'/processing/{pid}')
 
 
-def app_setup():
-    # If folder does not exist, mkdir the folder
+@app.route('/processing/<string:pid>', methods=['GET'])
+def processing(pid):
+    return render_template('processing.html', pid=pid)
 
-    if not os.path.isdir(UPLOAD_DIR):
-        os.mkdir(UPLOAD_DIR)
+
+@app.route('/get-results/<string:pid>', methods=['GET'])
+# @cache.cached(timeout=300)  # cache this image result for 5 minutes
+def get_results(pid):
+    if pid not in tasks:
+        return jsonify({"err": "No such task."})
+    task = tasks[pid]
+    process: Popen = task['process']
+
+    # !Ping the client when it's done
+
+    if process.poll() is None:
+        return jsonify({
+            'running': True
+        })
+        return jsonify({
+            'running': False,
+            'html': render_template('result.html', input_file=os.path.join(WEB_REL_UPLOAD_DIR, task['filename']), result_file=f'/assets/segmentingData/{task["filename"]}')
+        })
 
 
 if __name__ == '__main__':
-    app_setup()
-    app.run()
+    with app.app_context():
+        cache.clear()
+
+    # Create upload folder called /uploads
+    if not os.path.isdir(ABS_UPLOAD_DIR):
+        os.mkdir(ABS_UPLOAD_DIR)
+
+    app.run(port=5000, debug=True)
